cf89ed6c442f33738d01f0fdec9755d7439fd64e
Dane Edwards <Daneedw@gmail.com>
2022-10-13 20:27:50 -0500

    added filter

--- index.mjs
+++ index.mjs
@@ -2,14 +2,30 @@ import fs from "fs";
 import ytdl from "ytdl-core";
 
 import ytpl from "ytpl";
-
-const playlist = await ytpl("PLpePgEi-vLu8GODHPPhud4VylnIQaIdxk");
+//example url
+//https://www.youtube.com/playlist?list=PLpePgEi-vLu8GODHPPhud4VylnIQaIdxk
+const playlist = await ytpl("PLpePgEi-vLu9euPlaqf1vABjBYVWBrjQp", {});
 const videos = playlist.items;
 
 let current = 0;
-const timer = setInterval(() => {
+const timer = setInterval(downloadVideo, 20000);
+
+downloadVideo();
+
+
+function replaceValidString(str){
+
+ return str.replace(/\\/g, "&")
+  .replace(/\//g, "&")
+  .replace(/\?/g, "_")
+  .replace(/\|/g, "_")
+  .replace(/"/g, "_")
+
+}
+function downloadVideo() {
   if (current >= videos.length) {
     clearInterval(timer);
+    process.exit();
   }
   if (current < videos.length) {
     console.log(current);
@@ -18,22 +34,19 @@ const timer = setInterval(() => {
     console.log(dir);
     let title = videos[current].title;
 
-    let file = `${dir}/${title.replace(/\\/g, "&").replace(/\//g, "&")}.mp4`;
+    let file = `${dir}/${replaceValidString(title)}.mp4`;
     while (fs.existsSync(file)) {
       console.log(`${file} exists already!`);
 
       current++;
       title = videos[current].title;
-      file = `${dir}/${title
-        .replace(/\\/g, "&")
-        .replace(/\//g, "&")
-        .replace(/\?/g, "_")
-        }.mp4`;
+      file = `${dir}/${replaceValidString(title)}.mp4`;
     }
     console.log(`downloading ${title} as ${file}.mp4`);
 
+    //added audio and video filter, some videos don't have both, so this should fix that issue.
     try {
-      ytdl(`${videos[current].shortUrl}`).pipe(fs.createWriteStream(file));
+      ytdl(`${videos[current].shortUrl}`, {filter:"audioandvideo"}).pipe(fs.createWriteStream(file));
     } catch (error) {
       fs.appendFile(
         "log.txt",
@@ -45,4 +58,5 @@ const timer = setInterval(() => {
 
     current++;
   }
-}, 11000);
+}
+
